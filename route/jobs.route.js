f8a147be49192ad74a150b0754b190f22a370e6f
teddy <teddystwn48@gmail.com>
2024-09-27 15:17:27 +0700

    update

--- route/jobs.route.js
+++ route/jobs.route.js
@@ -7,16 +7,7 @@ const pusher = require("../helper/pusher");
 route.post("/jobs/add", controller.addJobs);
 route.get("/test/:id", async (req, res) => {
   let { id } = req.params;
-  //   await axios
-  //     .get("http://localhost:3000/update/1")
-  //     .then((data) => {
-  //       res.json(data);
-  //     })
-  //     .catch((err) => {
-  //       res.json(err);
-  //     });
-  // let a = await fetch("http://localhost:3000/update/1");
-  // a.status
+
   await pusher.trigger("reminder", "done", Number(id), () => {
     console.log("trigger success");
   });
