eda1a4e43cd7e87f4a5678da24bf22f0fed1d3f6
teddy <teddystwn48@gmail.com>
2024-09-30 15:00:45 +0700

    update

--- usecase/jobs.usecase.js
+++ usecase/jobs.usecase.js
@@ -43,18 +43,18 @@ const pushTrigger = async (data) => {
 
 const addToCron = async () => {
   // console.log("cek cron data", cronData);
-  try {
-    for (const element of cronData) {
-      cron.schedule(element.cron, async () => {
-        console.log("first");
-        pushTrigger(element.id);
-        await sendMail(element.name);
-        // await sendTelegramMessage(element.name);
-      });
-    }
-  } catch (error) {
-    console.log("error cron", error.message);
+  // try {
+  for (const element of cronData) {
+    cron.schedule(element.cron, async () => {
+      console.log("first");
+      pushTrigger(element.id);
+      await sendMail(element.name);
+      await sendTelegramMessage(element.name);
+    });
   }
+  // } catch (error) {
+  //   console.log("error cron", error.message);
+  // }
 };
 
 cron.schedule("0 1 * * *", () => {
